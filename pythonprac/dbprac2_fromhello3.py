9eb7f0f1eb31588db5f2ba712784ef344f3d9961
INYONG KIM <ohnyong87@gmail.com>
2023-06-29 20:12:31 +0900

    Commit 8
    웹 크롤링한 데이터를 DB에 넣는 실습
    웹 크롤링에 사용된 라이브러리
    	requests, beautifulsoup
    DB에 사용된 라이브러리
    	pymongo
    	certifi(mac 권한 관리 문제)
    
    요약
    python-db 연결(java에서 jdbc 스크립트 유사 개념)
    requests.get(url, ..)을 통해 데이터 크롤링
    soup.select()를 통해 '원하는' 데이터 추출
    select_one()을 통해 각 변수에 필드삽입
    db.col.insert_one()을 통해 db에 도큐먼트 전송(삽입,insert)

---
+++ pythonprac/dbprac2_fromhello3.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Python과 DB를 연결하는 코드(공통)
+# MongoDB를 사용할 준비가 되었다.
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://ohnyong:test@cluster0.lu7mz8j.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbsparta
+
+# 웹 크롤링 결과를 DB에 저장하자
+
+# 다음은 웹 크롤링 구현 부분이다.
+URL = "https://movie.daum.net/ranking/reservation"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(URL, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 위 링크 페이지의 전체 HTML이 아웃풋 된다
+# print(soup)
+
+# BeautifulSoup 라이브러리는 엄청 많은 HTML 코드 중에 우리가 원하는 특정 부분 을 빠르고 쉽게 필터링 해주는 라이브러리이다.
+# Title 데이터가 포함된 코드를 가져와보자
+title = soup.select_one('#mainContent > div > div.box_ranking > ol > li:nth-child(1) > div > div.thumb_cont > strong > a')
+print(title)
+# 그 중에서 텍스트만
+# print(title.text)
+# 혹시나 그 중에서 속성만 가져오고 싶은 경우
+# ( ['xxx'] -> dictionary 직접관계없다 bs개발자가 만든 문법이다.)
+# print(title['href'])
+
+
+# 내가원하는 데이터들이 리스트로 나열되어 있었다.
+# 1개가 아니라 li를 포함한 전체 리스트들을 가져와보자
+# 내가 원하는 값이 있었던 li:nth-child(1)가 아니라 li까지 스코프를 지정하자
+titles = soup.select('#mainContent > div > div.box_ranking > ol > li')
+# print(titles)
+
+# 리스트 중에 반복문으로 
+# li 코드 내용 중에 클래스가 a class="link_txt"로 되어있는 태그부분만 가져오자
+for li in titles:
+    rank = li.select_one('.rank_num').text
+    title = li.select_one('.link_txt').text.strip()
+    rate = li.select_one('.txt_grade').text.replace(',','')
+       ######## 이번 실습에서는 가져온것을 저장하자로 변경되었다. ########
+    # INSERT_ONE
+    # 저장 - 예시
+    # doc = {'name':'bobby','age':21}
+    # db.users.insert_one(doc)
+
+# 연결된 DB로 doc 도큐먼트 내용을 삽입 insert 하자.
+    doc = {
+        'rank':rank,
+        'title':title,
+        'rate':rate
+    }
+    db.movies.insert_one(doc)
+
+#Python에서는 줄바꿈(탭)을 잘 맞춰야 한다.
+#위 코드에서 데이터가 반복문을 돌지않고 1개만 삽입되어서 의문을 가졌는데
+#doc = { ... } 부분이 그 위의 for 반복문과 같은열에 있었다.
+#for 내부로 doc 부분을 옮기니 정상적으로 반복문의 내용을 수행 했다.
+
+ 
+
