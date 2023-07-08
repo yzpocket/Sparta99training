d233aed92ba3e5532d08cfe0347215bb149d410b
INYONG KIM <ohnyong87@gmail.com>
2023-07-08 21:15:18 +0900

    Flask Branch Commit 4
    [Flask] Flask Framework 05 첫 사용 (Get, Post 요청과 응답)
    
    index.html에서 이벤트-요청
    -> app.py에서 반환
    -> index.html에서 응답 전송-출력
    
    정확한 흐름은 POST방식 주석에 두 파일을 번갈아가면서 확인

--- projects/01.prac/app.py
+++ projects/01.prac/app.py
@@ -1,17 +1,36 @@
 # 통상적으로 Flask framework를 사용할 때 가장 기본이되는 python파일을 app.py로 명칭한다.
 # 변경 가능하나 통상적으로 사용하니 그대로 따라하자.
 # render_template 를 추가로 임포트 했다.
-from flask import Flask, render_template
+# request를 추가로 임포트 했다.
+# jsonify를 추가로 임포트 했다.
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
-
-@app.route('/mypage')
-def mypage():
 # render_template를 임포트한 뒤 다음처럼 root/templates/ 폴더 내 html을 가져올 수 있다.
    return render_template('index.html')
+
+# get 방식 요청 예시
+# @app.route('/test', methods=['GET'])
+# def test_get():
+# #    title_give라는 데이터가 있으면 title_receive에 넣는다.
+#    title_receive = request.args.get('title_give')
+# #    title_receive를 출력해보자.
+#    print(title_receive)
+#    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+# post 방식 요청 예시(데이터 흐름은 index.html [1]부터 시작해서 보기)
+# [4] 객체 formData가 이곳에 도착하고 
+@app.route('/test', methods=['POST'])
+# [5] test_post()가 실행된다
+def test_post():
+# [6] request.form에 따라 key값이 title_give인 value("블랙팬서")이 title_receive로 담아지고
+   title_receive = request.form['title_give']
+# [7] title_receive가 출력된다. (블랙팬서)   
+   print(title_receive)
+# [8] 출력까지 메서드 수행이 완료되었으니 jsonify로 dictionary 형태의 아래 {'key1':'value1', 'key2':'value2'}를 json 형태로 반환된다. [응답]
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5001,debug=True)
