71426ae4bceabc4be23cd47e9b55670a874b7ffc
INYONG KIM <ohnyong87@gmail.com>
2023-07-08 18:20:04 +0900

    Flask Branch Commit 4
    1. render_template Import
    2. /mypage 맵핑 메서드에 반환값 render_template사용
    	/templates/index.html을 보여준다.

--- projects/01.prac/app.py
+++ projects/01.prac/app.py
@@ -1,6 +1,7 @@
 # 통상적으로 Flask framework를 사용할 때 가장 기본이되는 python파일을 app.py로 명칭한다.
 # 변경 가능하나 통상적으로 사용하니 그대로 따라하자.
-from flask import Flask
+# render_template 를 추가로 임포트 했다.
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,7 +10,8 @@ def home():
 
 @app.route('/mypage')
 def mypage():
-   return '<html>Button</html>'
+# render_template를 임포트한 뒤 다음처럼 root/templates/ 폴더 내 html을 가져올 수 있다.
+   return render_template('index.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5001,debug=True)
